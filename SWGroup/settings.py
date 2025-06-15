137670addd981a586030c84403ce22110b555bc2
verstov <seli@verstov.pro>
2025-06-15 23:25:42 +0400

    add new django app

--- SWGroup/settings.py
+++ SWGroup/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
 
     'rest_framework',
     "rest_framework_simplejwt",
+
+    "mis",
 ]
 
 MIDDLEWARE = [
@@ -139,3 +141,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
