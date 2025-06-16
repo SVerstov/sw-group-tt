17f92cfb73391af3c8eb6a7e8cb4e46649d99a5e
verstov <seli@verstov.pro>
2025-06-16 10:38:30 +0400

    add redirect from root to API endpoint

--- SWGroup/urls.py
+++ SWGroup/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 """
 
 from django.contrib import admin
+from django.shortcuts import redirect
 from django.urls import path
 from django.urls.conf import include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("mis.urls")),
+    path("/", lambda request: redirect("/api/")),
 ]
